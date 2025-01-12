2b26b648115a2619e50423878b80a5b4c61346b9
HubertKor21 <155633636+HubertKor21@users.noreply.github.com>
2025-01-12 17:11:01 +0100

    sixth commit

--- src/Pages/Register.tsx
+++ src/Pages/Register.tsx
@@ -6,9 +6,3 @@ export function Register() {
     <Form route='dj-rest-auth/registration/' method="register"/>
     );
   }
-
-<<<<<<< HEAD
-export default Register;
-=======
-export default Register;
->>>>>>> fcfc39c36887dc88b3d41229a32d15394d7f3610
